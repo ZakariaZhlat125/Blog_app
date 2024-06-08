9b7c623362a0c2e7469007e1d2049755db3b4d48
Batoul Jdid <92179135+batoul25@users.noreply.github.com>
2024-06-08 19:42:37 +0300

    Update dependency_injection.dart

--- lib/ViewModel/dependency_injection.dart
+++ lib/ViewModel/dependency_injection.dart
@@ -1,6 +1,6 @@
 import 'package:app_blog/ViewModel/internet_connection/network_controller.dart';
 import 'package:get/get.dart';
-
+//زينب مثال شاهين
 class DependencyInjection{
   static void init(){
     Get.put<NetworkController>(NetworkController(),permanent: true);
