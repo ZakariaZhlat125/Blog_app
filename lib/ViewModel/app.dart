6ef5d627aca9909076fc12b99ca294514894497c
Batoul Jdid <92179135+batoul25@users.noreply.github.com>
2024-06-08 19:42:16 +0300

    Update app.dart

--- lib/ViewModel/app.dart
+++ lib/ViewModel/app.dart
@@ -2,7 +2,7 @@ import 'package:app_blog/View/resources/routes_manager.dart';
 import 'package:app_blog/View/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
+//زينب مثال شاهين
 class MyApp extends StatelessWidget {
   const MyApp._internal();
 
