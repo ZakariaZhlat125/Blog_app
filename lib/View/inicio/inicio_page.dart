d5fbe26a90d72ae11eaf629c6671308ea4d842c2
Batoul Jdid <92179135+batoul25@users.noreply.github.com>
2024-06-08 19:53:58 +0300

    Update inicio_page.dart

--- lib/View/inicio/inicio_page.dart
+++ lib/View/inicio/inicio_page.dart
@@ -7,7 +7,7 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'header.dart';
-
+//بتول يونس جديد
 class InicioPage extends StatefulWidget {
   const InicioPage({super.key});
 
