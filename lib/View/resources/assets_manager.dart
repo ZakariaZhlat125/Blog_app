0e0bd286559eb35b5f9030cb5a23ddebc7a09f61
Batoul Jdid <92179135+batoul25@users.noreply.github.com>
2024-06-08 19:57:02 +0300

    Update assets_manager.dart

--- lib/View/resources/assets_manager.dart
+++ lib/View/resources/assets_manager.dart
@@ -1,7 +1,7 @@
 const String ICONS_PATH = 'assets/icons';
 const String IMAGE_PATH = 'assets/images';
 const String JSON_PATH = 'assets/json';
-
+//عابد جرجي بربر
 sealed class AssetsManager{
 
   static const String menu = "$ICONS_PATH/menu-icon.png";
