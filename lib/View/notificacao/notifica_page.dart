8632865dc7ed8166f2bb2fc8bcc7a1363471ad8f
Batoul Jdid <92179135+batoul25@users.noreply.github.com>
2024-06-08 19:56:36 +0300

    Update notifica_page.dart

--- lib/View/notificacao/notifica_page.dart
+++ lib/View/notificacao/notifica_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 import '../resources/color_manager.dart';
-
+//روان محمد بركات
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
-
+  
   @override
   State<NotificationPage> createState() => _NotificationPageState();
 }
